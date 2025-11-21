8a035cc5f409aadb8d5df7e0a616d99554254a3c
chandhusarya <chandhu@sarya.ae>
2025-11-21 09:52:37 +0400

    module for wifi acess

---
+++ sarya_wifi/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright (C) Softhealer Technologies.
+{
+    "name": "Sarya Wifi",
+    "author": "Sarya",
+    "website": "www.sarya.com",
+    "category": "HR - ADMIN",
+    'license': 'AGPL-3',
+    "summary": "",
+    "version": "17.10",
+    "depends": ["hr"],
+
+    "data": [
+        'security/ir.model.access.csv',
+        "views/hr_employee_views.xml",
+    ],
+
+    "images": [],
+    "application": True,
+    "auto_install": False,
+    "installable": True,
+}
