f7f18d082c100cd3a17327dcd72bf5a295b0bfce
Pranav PS <pranav@sarya.ae>
2025-12-30 21:20:55 +0400

    [FIX]checklist

---
+++ sarya_checklist/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright (C) Softhealer Technologies.
+{
+    "name": "Sarya Checklist",
+    "author": "Sarya",
+    "website": "www.sarya.com",
+    "category": "POS",
+    'license': 'AGPL-3',
+    "summary": "",
+    "version": "17.10",
+    "depends": ['base', 'mail', 'point_of_sale'],
+
+    "data": [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'views/checklist_config_views.xml',
+        'views/responsible_master_views.xml',
+        'views/outlet_checklist_views.xml',
+        'views/menu.xml',
+        'data/cron.xml',
+    ],
+
+    "images": [],
+    "application": True,
+    "auto_install": False,
+    "installable": True,
+}
